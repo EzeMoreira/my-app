9e8710a7cedab0f735a51dbf940af2e5dcf3afbf
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-11 12:50:30 -0300

    agregado ejemplos con let fuera del return

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import imagen from './ceros-unos.png';
 import './App.css';
 
 function App() {
+
+  let num = 1;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={imagen} className="App-logo" alt="logo" />
-        
+        <p>Esta imagen es la numero {num}</p>
       </header>
     </div>
   );
