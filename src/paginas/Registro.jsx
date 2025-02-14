0d61a73ee7166d1d3d37f563fc47bd1494ef3385
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-14 12:12:40 -0300

    add info en registro jsx

---
+++ src/paginas/Registro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Registro(){
+
+    return(
+        <div>
+            <h1>Registarse para continuar</h1>
+        </div>
+    );
+}
+
+export default Registro;
