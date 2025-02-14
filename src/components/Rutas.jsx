6d353f9db31fd81b9252e5789f56026f0fc8787a
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-14 12:41:07 -0300

    rutas definidas mas info agreagada

--- src/components/Rutas.jsx
+++ src/components/Rutas.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Home from "../paginas/Home";
+import Loguearse from "../paginas/Loguearse";
+import Registro from "../paginas/Registro";
+import {Routes, Route} from 'react-router-dom'
 
 function Rutas(){
 
     return(
-        <div></div>
+        <Routes>
+	        <Route path="/" element= {<Home />} />
+            <Route path="/Loguearse" element= {<Loguearse />} />
+            <Route path="/Registro" element= {<Registro />} />
+        </Routes>
     );
 }
 
