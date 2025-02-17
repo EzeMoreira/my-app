30139b49f819a85542346ad67c78dbdd71a89942
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-17 14:10:12 -0300

    modificacion en rutas, y estilo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import Home from './paginas/Home';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Rutas from './components/Rutas';
+import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
 
   return (
       <Router>
-	        <Rutas />
+	        <div className="App">
+            <nav>
+              <ul>
+                <Link to="/" class="list">
+                  Home
+                </Link>
+                <Link to="/Registro" class="list">
+                  Registro
+                </Link>
+                <Link to="/Loguearse" class="list">
+                  Loguearse
+                </Link>
+              </ul>
+            </nav>
+          </div>
       </Router>
   );
 }
