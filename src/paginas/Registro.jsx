055404fd747a191f0c86045c286b6dbff677c87a
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-18 12:48:27 -0300

    modificaciones en registro y estilos

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -4,17 +4,17 @@ function Registro(){
 
     return(
         <>
-            <h1>Registrate para novedades</h1>
+            <h1 class="formulario">Registrate para novedades</h1>
         <div>
             <form action="#" method="GET">
                 <label>
-                    Nombre completo:
+                    Nombre completo: <br/>
                     <input type="text" name="name" required/>
-                </label> <br />
-                <label>
-                    correo electronico
+                </label> <br/>
+                <label> <br/>
+                    correo electronico <br/>
                     <input type="email" name="email" required/>
-                </label> <br />
+                </label> <br/> <br/> 
                 <input type="submit" value="Submit"/>
             </form>
         </div>
