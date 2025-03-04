ab86ae45d079f78893c61220014cde83d866fd5a
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-04 12:38:40 -0300

    primera parte de la colocacion de hooks en productos y creacion de carpeta y archivo productos servicios

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 function Productos(){
     
+    const [Productos, setProductos] = useState();
+
+    useEffect(() =>{
+        const request = async () => {
+            try {
+                const querySnapshot = await getAllProductos();
+                console.log(querySnapshot.docs)
+            };
+            setProductos(querySnapshot.docs);
+        }   catch (e) {
+            console.log(e);
+        }
+    };
+    request();
+}, []);
+
     return(
         <div>
             <h1>Muestra de los Productos</h1>
