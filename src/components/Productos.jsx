fc9c06b66ad083b0762b95ceb7a64e835811998f
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-04 14:26:31 -0300

    modificacion en ProductoServicio y en productos jsx

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect} from "react";
+import { getAllProductos } from "../servicios/ProductoServicio";
 
 function Productos(){
     
     const [Productos, setProductos] = useState();
 
-    useEffect(() =>{
+    useEffect(() => {
         const request = async () => {
             try {
                 const querySnapshot = await getAllProductos();
-                console.log(querySnapshot.docs)
-            };
-            setProductos(querySnapshot.docs);
+                console.log(querySnapshot.docs);
+                setProductos(querySnapshot.docs);
         }   catch (e) {
             console.log(e);
         }
--- src/servicios/ProductoServicio.jsx
+++ src/servicios/ProductoServicio.jsx
@@ -1,1 +1,8 @@
 import React from "react";
+
+export function getAllProductos(){
+
+    let id = "Iphone";
+
+    return fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${id}`).then(res=>res.json());
+}
