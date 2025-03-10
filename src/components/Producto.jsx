0c7a0e6c32b8a50167a299a4881f47a5147898d8
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-10 19:26:52 -0300

    creacion de boton mas funciones handleComprar

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Producto({id, imagen, nombre, precio}){
 
+    const [comprar, setComprar] = useState("");
+    const handleComprar = (e) => {
+        setComprar("Producto comprado")
+    }
     return(
         <div className="contenedorProducto">
             <img src={imagen} alt="Imagen del producto" />
             <div className="nombreYprecio">
             <h2>{nombre}</h2>
             <h3>AR${precio}</h3>
-            </div>
+            </div> <br />
+            <div> {comprar} </div>
+            <button onClick={handleComprar}>Comprar</button>
         </div>
     )
 }
