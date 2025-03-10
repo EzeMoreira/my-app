b54eec09f186225d602120c6132814d9867bdd61
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-10 18:57:46 -0300

    creacion de clases dentro de divs producto jsx

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -3,10 +3,12 @@ import React from "react";
 function Producto({id, imagen, nombre, precio}){
 
     return(
-        <div>
+        <div className="contenedorProducto">
             <img src={imagen} alt="Imagen del producto" />
+            <div className="nombreYprecio">
             <h2>{nombre}</h2>
             <h3>AR${precio}</h3>
+            </div>
         </div>
     )
 }
