6541bf746dff768d7500158d1510fe131da43b25
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-11 15:52:53 -0300

    modificacion en loguearse

--- src/paginas/Loguearse.jsx
+++ src/paginas/Loguearse.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
+
 function Loguearse() {
 
-    return( 
-
+    return(
         <div className="registro">
             <h1>Loggin</h1>
             <form action="#" method="GET">
@@ -23,20 +23,3 @@ function Loguearse() {
 }
 
 export default Loguearse;
-
-
- // <h1 class="login">Loguearse</h1>
-        // <div>
-        //     <form action="#" method="GET">
-        //         <label> <br/>
-        //             Correo electronico <br/>
-        //             <input type="email" name="email" placeholder="Correo electronico" required/>
-        //         </label> <br/> 
-        //         <label> <br/>
-        //             Password 
-        //             </label> <br/>
-        //             <input type="password" name="password" placeholder="password" required />
-        //             <br/>  <br/>
-        //             <button id="botonComprar" type="submit">Enviar</button>
-        //     </form>
-        // </div>
