08303bbb0570e0198b5a696a1e50d339534c1ef2
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-11 15:51:12 -0300

    modificacion en rutas.jsx

--- src/components/Rutas.jsx
+++ src/components/Rutas.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import Loguearse from "../paginas/Loguearse";
 import Registro from "../paginas/Registro";
 import Home from "../paginas/Home";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-function Rutas(){
+function Rutas() {
 
-    return(
+    return (
         <Routes>
-	        <Route path="/" element= {<Home />} />
-            <Route path="/Loguearse" element= {<Loguearse />} />
-            <Route path="/Registro" element= {<Registro />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Loguearse" element={<Loguearse />} />
+            <Route path="/Registro" element={<Registro />} />
         </Routes>
     );
 }
-
 export default Rutas;
