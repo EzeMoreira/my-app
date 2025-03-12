e26f05db2575b65307b3af93e7784480b1aca526
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-12 12:05:09 -0300

    modificacion en loguearse jsx y css modificado con button y button hover

--- src/paginas/Loguearse.jsx
+++ src/paginas/Loguearse.jsx
@@ -4,21 +4,23 @@ import React from "react";
 function Loguearse() {
 
     return(
-        <div className="registro">
-            <h1>Loggin</h1>
+        <>
+        <h1>Login</h1>
+            <div className="registro">
             <form action="#" method="GET">
                     <label>
                         Email:
                     </label><br />
                     <input type="email" name="email" required placeholder="email"/><br />
-                    <label>
+                    <label> <br />
                         Password:
                     </label><br />
                     <input type="password" name="password" placeholder="password" required />
-                    <br />
+                    <br /> <br />
                     <button type="submit" id="botonComprar" >Enviar</button><br />
                 </form>
         </div>
+        </>
     );
 }
 
